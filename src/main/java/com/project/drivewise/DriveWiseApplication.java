dd578603dda8c7a56e398466170ebb6ec34d6e0c
T490s <nikoletabrsv99@abv.bg>
2025-12-29 20:49:22 +0200

    Init Spring boot project.

---
+++ src/main/java/com/project/drivewise/DriveWiseApplication.java
@@ -0,0 +1,13 @@
+package com.project.drivewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/drivewise/DriveWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.drivewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
